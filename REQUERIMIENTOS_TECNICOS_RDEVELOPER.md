72e1973a55eee16e187936009b8b196472d2c27f
Renzo Cáceres Rossi <arenzocaceresrossi@gmail.com>
2024-11-17 00:39:02 -0500

    upload presentation files

---
+++ REQUERIMIENTOS_TECNICOS_RDEVELOPER.md
@@ -0,0 +1,20 @@
+## Latin R 2024
+## Desarrollo de paquetes (Packages) en R: Desde la Idea hasta la Implementación y Distribución
+## Ponente: Renzo Cáceres Rossi (arenzocaceresrossi@gmail.com)
+
+### urls a utilizar en la demostración
+
+- CRAN: https://cran.r-project.org/
+
+- R CRAN TASK VIEW: https://r-packages.io/task-views
+
+- CRAN INCOMING DASHBOARD: https://r-hub.github.io/cransays/articles/dashboard.html
+
+### REQUERIMIENTOS TECNICOS
+
+- R Programming Language Download R-4.4.2 for Windows (83 megabytes, 64 bit), Linux o Mac
+(Se recomienda última versión de R)
+
+- RStudio (Última versión se recomienda)
+
+- RTools (Se recomienda última versión)
---
+++ packages_Rdeveloper_001.R
@@ -0,0 +1,45 @@
+# Latin R 2024
+# Desarrollo de paquetes (Packages) en R: Desde la Idea hasta la Implementación y Distribución
+# Ponente: Renzo Cáceres Rossi (arenzocaceresrossi@gmail.com)
+
+# Activate Developer Packages
+
+library(available)
+
+# install.packages("available")
+
+library(devtools)
+
+# install.packages("devtools")
+
+library(usethis)
+
+# install.packages("usethis")
+
+library(testthat)
+
+# install.packages("testthat")
+
+library(pkgdown)
+
+# install.packages("pkgdown")
+
+# Load data sets packages
+
+library(datasets)
+
+# already part of the R programming language
+
+library(timeSeriesDataSets)
+
+# install.packages("timeSeriesDataSets")
+
+library(usdatasets)
+
+# install.packages("usdatasets")
+
+library(MedDataSets)
+
+# install.packages("MedDataSets")
+
+
